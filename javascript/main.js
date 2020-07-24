c226610e9f8f540d15cc4d1fc99f20858befbe3d
Brian Kiefer <bakief04@gmail.com>
2020-07-24 17:43:30 -0400

    fixed typos

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,20 @@
 //Weight Conversions
-
 document.getElementById("output").style.visibility = "hidden";
 document.getElementById("lbsInput").addEventListener("input", function(e) {
-  document.getElementById("output").style.visibility = "visible";
-  let oz = e.target.value;
-  document.getElementById("ozOutput").innerHTML = oz * 1;
-  document.getElementById("gramsOutput").innerHTML = oz * 128;
-  document.getElementById("cupsOutput").innerHTML = oz / 8;
+document.getElementById("output").style.visibility = "visible";
+
+let oz = e.target.value;
+document.getElementById("ozOutput").innerHTML = oz * 1;
+document.getElementById("gramsOutput").innerHTML = oz * 128;
+document.getElementById("cupsOutput").innerHTML = oz / 8;
 });
 
 // Hamburger Menu
 
-// function toggleClass() {
-//     let menu = document.querySelector(".main-nav");
-//     menu.classList.toggle("toggleCls");
-// }
+function toggleClass() {
+    let menu = document.querySelector(".main-nav");
+    menu.classList.toggle("toggleCls");
+}
 
-// let hamburger = document.querySelector(".hamIcon");
-// hamburger.addEventListener("click", toggleClass);
+let hamburger = document.querySelector(".hamIcon");
+hamburger.addEventListener("click", toggleClass);
