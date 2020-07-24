0d7f38d484982b5d33029036699a0d895970651a
Brian Kiefer <bakief04@gmail.com>
2020-07-24 17:14:23 -0400

    added a weight conversion in javascript

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 This is my project, Hope you like it.
 
-The features I implimented are a responsive mobile navigation; 
-a grid layout that sizes a nav bar, paragraph and image elements, and a footer; 
-a hamburger style menu for mobile menus written in javascript;
-a form validation feature that checks if you hjave used a valid email.
-
+The features I implimented are a responsive mobile navigation. 
+A grid layout that sizes a nav bar, paragraph and image elements, and a footer.
+A hamburger style menu for mobile menus written in javascript.
+And a weight conversion that takes ounces and converts it into grams, and cups for purposes of reading recipe amounts easier.
--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,20 @@
-// hamburger menu
-function toggleClass() {
-    let menu = document.querySelector(".main-nav");
-    menu.classList.toggle("toggleCls");
-}
+//Weight Conversions
 
-let hamburger = document.querySelector(".hamIcon");
-hamburger.addEventListener("click", toggleClass);
+document.getElementById("output").style.visibility = "hidden";
+document.getElementById("lbsInput").addEventListener("input", function(e) {
+  document.getElementById("output").style.visibility = "visible";
+  let oz = e.target.value;
+  document.getElementById("ozOutput").innerHTML = oz * 1;
+  document.getElementById("gramsOutput").innerHTML = oz * 128;
+  document.getElementById("cupsOutput").innerHTML = oz / 8;
+});
+
+// Hamburger Menu
+
+// function toggleClass() {
+//     let menu = document.querySelector(".main-nav");
+//     menu.classList.toggle("toggleCls");
+// }
+
+// let hamburger = document.querySelector(".hamIcon");
+// hamburger.addEventListener("click", toggleClass);
